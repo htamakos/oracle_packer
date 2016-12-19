bffbdf0dd004fb16c0caeaac1856ce04f1564f0b
htamakos <htamakos>
2016-12-19 09:49:00 +0900

    delete virtualbox guestaddtions settings

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -7,8 +7,10 @@ echo "create ${SSH_USER_HOME}/.ssh dir"
 mkdir -pm 700 ${SSH_USER_HOME}/.ssh
 
 VAGRANT_SSH_KEY=`curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub`
+
 echo $VAGRANT_SSH_KEY
 echo "${VAGRANT_SSH_KEY}" >> ${SSH_USER_HOME}/.ssh/authorized_keys
 
 chmod 0600 ${SSH_USER_HOME}/.ssh/authorized_keys
 chown -R ${SSH_USER}:${SSH_USER} ${SSH_USER_HOME}/.ssh
+
