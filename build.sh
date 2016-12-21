e70b9af30a86ad49078970b1d9fed8956873e957
htamakos <htamakos>
2016-12-21 17:38:33 +0900

    set proxy

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -euxC
 
-#public_key=`cat ~/.ssh/id_rsa.pub`
-rm box/oraclelinux68.box || true
-packer build oraclelinux.json #-var "authorized_public_key=${public_key}"  
-vagrant box remove oraclelinux68 || true
-vagrant box add box/oraclelinux68.box --name oraclelinux68 || true
+BOX_NAME=${1-oraclelinux68}
+SET_PROXY=${SET_PROXY-false}
+rm box/${BOX_NAME}.box || true
+
+if [ $SET_PROXY = 'true' ] ;then
+  echo "set proxy: $http_proxy"
+  packer build -var "outputbox_name=$BOX_NAME" -var "vm_name=$BOX_NAME" -var "PACKER_HTTP_PROXY=$http_proxy" oraclelinux.json
+else
+  packer build  -var "outputbox_name=$BOX_NAME" -var "vm_name=$BOX_NAME" oraclelinux.json
+fi
+vagrant box remove ${BOX_NAME} || true
+vagrant box add box/${BOX_NAME}.box --name ${BOX_NAME} || true
--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,4 +1,4 @@
-#!bin/bash -euxC
+#!/bin/bash -euxC
 
 SSH_USER=${SSH_USER:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
@@ -6,10 +6,16 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 echo "create ${SSH_USER_HOME}/.ssh dir"
 mkdir -pm 700 ${SSH_USER_HOME}/.ssh
 
-VAGRANT_SSH_KEY=`curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub`
+SSH_KEY_URL="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub"
+
+PACKER_HTTP_PROXY=`echo $PACKER_HTTP_PROXY | sed -e "s/^\(http\|https\):\/\///g"`
+[ PACKER_HTTP_PROXY != "" ] && P_PROXY="-x $PACKER_HTTP_PROXY" || P_PROXY="" 
+
+VAGRANT_SSH_KEY=`eval "curl $P_PROXY $SSH_KEY_URL"`
 
 echo $VAGRANT_SSH_KEY
 echo "${VAGRANT_SSH_KEY}" >> ${SSH_USER_HOME}/.ssh/authorized_keys
+cat ${SSH_USER_HOME}/.ssh/authorized_keys
 
 chmod 0600 ${SSH_USER_HOME}/.ssh/authorized_keys
 chown -R ${SSH_USER}:${SSH_USER} ${SSH_USER_HOME}/.ssh
