9009ca209d01a18974865c7e50eea936961cecd2
htamakos <htamakos>
2016-12-22 14:51:35 +0900

    fix typo

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,7 +5,7 @@ chmod -R a+w /var/log/script/
 
 tee -a /etc/profile <<-"EOF"
 # output command log
-P_PROC=`px aux | grep $PPID | grep sshd | awk '{ print $11 }'`
+P_PROC=`ps aux | grep $PPID | grep sshd | awk '{ print $11 }'`
 
 if [ "$P_PROC" = sshd: ]; then
   script -q /var/log/script/`date '+%Y_%m_%d_%H_%M_%S'`.log
--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -5,18 +5,18 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
 echo "create ${SSH_USER_HOME}/.ssh dir"
 mkdir -pm 700 ${SSH_USER_HOME}/.ssh
+touch ${SSH_USER_HOME}/.ssh/authorized_keys
 
 SSH_KEY_URL="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub"
 
 PACKER_HTTP_PROXY=`echo $PACKER_HTTP_PROXY | sed -e "s/^\(http\|https\):\/\///g"`
 [ PACKER_HTTP_PROXY != "" ] && P_PROXY="-x $PACKER_HTTP_PROXY" || P_PROXY="" 
 
-VAGRANT_SSH_KEY=`eval "curl $P_PROXY $SSH_KEY_URL"`
+eval "curl -L -o $SSH_USER_HOME/.ssh/authorized_keys $P_PROXY $SSH_KEY_URL"
 
-echo $VAGRANT_SSH_KEY
-echo "${VAGRANT_SSH_KEY}" >> ${SSH_USER_HOME}/.ssh/authorized_keys
 cat ${SSH_USER_HOME}/.ssh/authorized_keys
 
 chmod 0600 ${SSH_USER_HOME}/.ssh/authorized_keys
 chown -R ${SSH_USER}:${SSH_USER} ${SSH_USER_HOME}/.ssh
+
 
